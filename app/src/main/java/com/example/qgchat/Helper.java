46aa082a0ce149d0fc315b72e6a9d1286ee459ae
mss <mashanshui@qq.com>
2018-02-23 15:21:13 +0800

    添加头像显示功能

---
+++ app/src/main/java/com/example/qgchat/Helper.java
@@ -0,0 +1,185 @@
+package com.example.qgchat;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Message;
+
+import com.example.qgchat.activity.NewFriendsMsgActivity;
+import com.example.qgchat.bean.StatusResponse;
+import com.example.qgchat.cache.UserCacheManager;
+import com.example.qgchat.db.DBInviteMessage;
+import com.example.qgchat.util.HttpUtil;
+import com.example.qgchat.util.PreferencesUtil;
+import com.google.gson.Gson;
+import com.hyphenate.EMContactListener;
+import com.hyphenate.chat.EMClient;
+import com.hyphenate.chat.EMMessage;
+import com.hyphenate.easeui.EaseConstant;
+import com.hyphenate.easeui.EaseUI;
+import com.hyphenate.easeui.domain.EaseUser;
+import com.hyphenate.easeui.model.EaseNotifier;
+import com.hyphenate.easeui.utils.EaseCommonUtils;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.Response;
+
+/**
+ * Created by Administrator on 2018-02-23.
+ */
+
+public class Helper {
+    private static Helper instance = null;
+    private EaseUI easeUI;
+    private Context appContext;
+    private PreferencesUtil preferencesUtil;
+
+    public synchronized static Helper getInstance() {
+        if (instance == null) {
+            instance = new Helper();
+        }
+        return instance;
+    }
+
+    public void init(Context context) {
+        appContext = context;
+        preferencesUtil = new PreferencesUtil(context);
+        easeUI = EaseUI.getInstance();
+        setEaseUIProviders();
+        setContactListener();
+    }
+
+    private void setContactListener() {
+        EMClient.getInstance().contactManager().setContactListener(new MyContactListener());
+    }
+
+    private void setEaseUIProviders() {
+        // set profile provider if you want easeUI to handle avatar and nickname
+        easeUI.setUserProfileProvider(new EaseUI.EaseUserProfileProvider() {
+
+            @Override
+            public EaseUser getUser(String username) {
+                return getUserInfo(username);
+            }
+        });
+    }
+
+    private EaseUser getUserInfo(String username){
+        // To get instance of EaseUser, here we get it from the user list in memory
+        // You'd better cache it if you get it from your server
+
+        // 从本地缓存中获取用户昵称头像
+        EaseUser user = UserCacheManager.getEaseUser(username);
+
+//        if(username.equals(EMClient.getInstance().getCurrentUser()))
+//            return getUserProfileManager().getCurrentUserInfo();
+//        user = getContactList().get(username);
+//        if(user == null && getRobotList() != null){
+//            user = getRobotList().get(username);
+//        }
+
+        // if user is not in your contacts, set inital letter for him/her
+        if(user == null){
+            user = new EaseUser(username);
+            EaseCommonUtils.setUserInitialLetter(user);
+        }
+        return user;
+    }
+
+    public class MyContactListener implements EMContactListener {
+        @Override
+        public void onContactAdded(String username) {
+            appContext.sendBroadcast(new Intent(EaseConstant.ACTION_CONTACT_CHANAGED));
+//            refreshUIWithContacts();
+        }
+
+        @Override
+        public void onContactDeleted(final String username) {
+            appContext.sendBroadcast(new Intent(EaseConstant.ACTION_CONTACT_CHANAGED));
+//            refreshUIWithContacts();
+        }
+
+        @Override
+        public void onContactInvited(final String username, String reason) {
+            DBInviteMessage new_friend = new DBInviteMessage();
+            new_friend.setStatus(DBInviteMessage.BEINVITEED);
+            new_friend.setReason(reason);
+            new_friend.setTime(System.currentTimeMillis());
+            new_friend.setFrom(username);
+            new_friend.save();
+            preferencesUtil.setUnreadMsgCount(preferencesUtil.getUnreadMsgCount() + 1);
+            EMMessage emMessage = EMMessage.createReceiveMessage(EMMessage.Type.TXT);
+            EaseUI.getInstance().getNotifier().setNotificationInfoProvider(new EaseNotifier.EaseNotificationInfoProvider() {
+                @Override
+                public String getDisplayedText(EMMessage message) {
+                    return username+"请求添加你为好友";
+                }
+
+                @Override
+                public String getLatestText(EMMessage message, int fromUsersNum, int messageNum) {
+                    return username+"请求添加你为好友";
+                }
+
+                @Override
+                public String getTitle(EMMessage message) {
+                    return "好友请求";
+                }
+
+                @Override
+                public int getSmallIcon(EMMessage message) {
+                    return 0;
+                }
+
+                @Override
+                public Intent getLaunchIntent(EMMessage message) {
+                    return new Intent(appContext, NewFriendsMsgActivity.class);
+                }
+            });
+            EaseUI.getInstance().getNotifier().onNewMsg(emMessage);
+//            refreshUIWithContacts();
+            appContext.sendBroadcast(new Intent(EaseConstant.ACTION_CONTACT_CHANAGED));
+        }
+
+        @Override
+        public void onFriendRequestAccepted(String username) {
+            DBInviteMessage new_friend = new DBInviteMessage();
+            new_friend.setStatus(DBInviteMessage.BEAGREED);
+            new_friend.setTime(System.currentTimeMillis());
+            new_friend.setFrom(username);
+            new_friend.save();
+//            refreshUIWithContacts();
+            appContext.sendBroadcast(new Intent(EaseConstant.ACTION_CONTACT_CHANAGED));
+            addFriend(preferencesUtil.getAccount(),username);
+        }
+
+        @Override
+        public void onFriendRequestDeclined(String username) {
+            DBInviteMessage new_friend = new DBInviteMessage();
+            new_friend.setStatus(DBInviteMessage.BEREFUSED);
+            new_friend.setTime(System.currentTimeMillis());
+            new_friend.setFrom(username);
+            new_friend.save();
+        }
+
+    }
+
+    private void addFriend(String ownerAccount,String friendAccount) {
+        String url = HttpUtil.addFriendURL + "?ownerAccount=" + ownerAccount + "&friendAccount=" + friendAccount;
+        HttpUtil.sendOkHttpRequest(url, new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                Message message = new Message();
+                String responseData = response.body().string();
+                Gson gson = new Gson();
+                StatusResponse statusResponse = gson.fromJson(responseData, StatusResponse.class);
+            }
+        });
+    }
+}
--- app/src/main/java/com/example/qgchat/MyApplication.java
+++ app/src/main/java/com/example/qgchat/MyApplication.java
@@ -2,32 +2,14 @@ package com.example.qgchat;
 
 import android.app.Application;
 import android.content.Context;
-import android.content.Intent;
-import android.os.Message;
 
-import com.example.qgchat.activity.NewFriendsMsgActivity;
-import com.example.qgchat.bean.StatusResponse;
-import com.example.qgchat.db.DBInviteMessage;
-import com.example.qgchat.util.HttpUtil;
-import com.example.qgchat.util.PreferencesUtil;
-import com.google.gson.Gson;
-import com.hyphenate.EMContactListener;
 import com.hyphenate.chat.EMClient;
-import com.hyphenate.chat.EMMessage;
 import com.hyphenate.chat.EMOptions;
-import com.hyphenate.easeui.EaseConstant;
 import com.hyphenate.easeui.EaseUI;
-import com.hyphenate.easeui.model.EaseNotifier;
 import com.iflytek.cloud.SpeechConstant;
 import com.iflytek.cloud.SpeechUtility;
 
 import org.litepal.LitePal;
-
-import java.io.IOException;
-
-import okhttp3.Call;
-import okhttp3.Callback;
-import okhttp3.Response;
 
 /**
  * Created by Administrator on 2017/12/7.
@@ -35,13 +17,13 @@ import okhttp3.Response;
 
 public class MyApplication extends Application {
     public static Context applicationContext;
-    private PreferencesUtil preferencesUtil;
+    private static MyApplication instance;
 
     @Override
     public void onCreate() {
         super.onCreate();
         applicationContext = this;
-        preferencesUtil = new PreferencesUtil(this);
+        instance = this;
         SpeechUtility.createUtility(this, SpeechConstant.APPID + "=59fd1d6b");
         LitePal.initialize(this);
 
@@ -57,102 +39,13 @@ public class MyApplication extends Application {
         //在做打包混淆时，关闭debug模式，避免消耗不必要的资源
         EMClient.getInstance().setDebugMode(true);
 
-        EMClient.getInstance().contactManager().setContactListener(new MyContactListener());
+        Helper.getInstance().init(applicationContext);
         
     }
 
-    public class MyContactListener implements EMContactListener {
-        @Override
-        public void onContactAdded(String username) {
-            sendBroadcast(new Intent(EaseConstant.ACTION_CONTACT_CHANAGED));
-//            refreshUIWithContacts();
-        }
-
-        @Override
-        public void onContactDeleted(final String username) {
-            sendBroadcast(new Intent(EaseConstant.ACTION_CONTACT_CHANAGED));
-//            refreshUIWithContacts();
-        }
-
-        @Override
-        public void onContactInvited(final String username, String reason) {
-            DBInviteMessage new_friend = new DBInviteMessage();
-            new_friend.setStatus(DBInviteMessage.BEINVITEED);
-            new_friend.setReason(reason);
-            new_friend.setTime(System.currentTimeMillis());
-            new_friend.setFrom(username);
-            new_friend.save();
-            preferencesUtil.setUnreadMsgCount(preferencesUtil.getUnreadMsgCount() + 1);
-            EMMessage emMessage = EMMessage.createReceiveMessage(EMMessage.Type.TXT);
-            EaseUI.getInstance().getNotifier().setNotificationInfoProvider(new EaseNotifier.EaseNotificationInfoProvider() {
-                @Override
-                public String getDisplayedText(EMMessage message) {
-                    return username+"请求添加你为好友";
-                }
-
-                @Override
-                public String getLatestText(EMMessage message, int fromUsersNum, int messageNum) {
-                    return username+"请求添加你为好友";
-                }
-
-                @Override
-                public String getTitle(EMMessage message) {
-                    return "好友请求";
-                }
-
-                @Override
-                public int getSmallIcon(EMMessage message) {
-                    return 0;
-                }
-
-                @Override
-                public Intent getLaunchIntent(EMMessage message) {
-                    return new Intent(applicationContext, NewFriendsMsgActivity.class);
-                }
-            });
-            EaseUI.getInstance().getNotifier().onNewMsg(emMessage);
-//            refreshUIWithContacts();
-            sendBroadcast(new Intent(EaseConstant.ACTION_CONTACT_CHANAGED));
-        }
-
-        @Override
-        public void onFriendRequestAccepted(String username) {
-            DBInviteMessage new_friend = new DBInviteMessage();
-            new_friend.setStatus(DBInviteMessage.BEAGREED);
-            new_friend.setTime(System.currentTimeMillis());
-            new_friend.setFrom(username);
-            new_friend.save();
-//            refreshUIWithContacts();
-            sendBroadcast(new Intent(EaseConstant.ACTION_CONTACT_CHANAGED));
-            addFriend(preferencesUtil.getAccount(),username);
-        }
-
-        @Override
-        public void onFriendRequestDeclined(String username) {
-            DBInviteMessage new_friend = new DBInviteMessage();
-            new_friend.setStatus(DBInviteMessage.BEREFUSED);
-            new_friend.setTime(System.currentTimeMillis());
-            new_friend.setFrom(username);
-            new_friend.save();
-        }
-
+    public static MyApplication getInstance() {
+        return instance;
     }
 
-    private void addFriend(String ownerAccount,String friendAccount) {
-        String url = HttpUtil.addFriendURL + "?ownerAccount=" + ownerAccount + "&friendAccount=" + friendAccount;
-        HttpUtil.sendOkHttpRequest(url, new Callback() {
-            @Override
-            public void onFailure(Call call, IOException e) {
 
-            }
-
-            @Override
-            public void onResponse(Call call, Response response) throws IOException {
-                Message message = new Message();
-                String responseData = response.body().string();
-                Gson gson = new Gson();
-                StatusResponse statusResponse = gson.fromJson(responseData, StatusResponse.class);
-            }
-        });
-    }
 }
